ff328181d1963466f102675129e9471ca897447d
kagxin <k506855660@163.com>
2018-01-17 13:37:42 +0800

    hello world ex

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,19 @@
 import asyncio
+import functools
 
+def hello_wrold(loop, arg):
+    print(arg)
+    loop.stop()
+
+
+def main():
+    loop = asyncio.get_event_loop()   # 获取事件循环
+    loop.set_debug(True)    #打开debug模式
+    # loop.call_soon(hello_wrold, loop, 'hello world!')  
+    loop.call_soon(functools.partial(hello_wrold, loop=loop, arg='hello world!'))  #向事件循环中注册回调函数，使用偏函数，显示keyword
+    loop.run_forever()
+    loop.close()
+
+
+if __name__ == '__main__':
+    main()
