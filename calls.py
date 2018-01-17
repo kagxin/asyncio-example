c4500175c1670070f6a098b796c6adf80ab9b38d
康鑫 <k506855660@163.com>
2018-01-17 23:25:26 +0800

    注册回调函数,监控描述符事件.

---
+++ calls.py
@@ -0,0 +1,42 @@
+import asyncio
+import datetime
+
+
+def hello_world():
+    print(datetime.datetime.now(), ' ', end='')
+    print('Hello World')
+
+
+def print_delay(arg, handle):
+    print(datetime.datetime.now(), ' ', end='')
+    print(arg)
+    handle.cancel()  # 通过handle删除注册函数
+
+
+def print_at(arg, loop):
+    print(datetime.datetime.now(), ' ', end='')
+    print(arg)
+    loop.stop()
+
+def stop_loop(loop):
+    print(datetime.datetime.now(), ' timeout stop loop.')
+    loop.stop()
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+    print(loop.time())
+
+    handle = loop.call_at(loop.time() + 10, print_at, 'print_at.', loop)  # 在10s之后运行
+    loop.call_soon(hello_world)  # 时间循环开始运行尽快调用hell_world.
+    loop.call_later(5, print_delay, 'print_delay.', handle)  # 注册事件之后5s运行
+
+
+    loop.call_at(loop.time()+20, stop_loop, loop)
+    loop.run_forever()
+    loop.close()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ watch_fd.py
@@ -0,0 +1,45 @@
+import asyncio
+import socket
+
+
+HOST = 'localhost'
+PORT = 8003
+ADDR = (HOST, PORT)
+
+
+def print_read_data(sock, loop):  # 读事件的回调函数
+    data = sock.recv(1024)
+    print(data.decode())
+
+
+def stop_loop(loop, sock):
+    sock.close()
+    loop.stop()
+
+
+def main():
+    sock = socket.socket()
+    sock.bind(ADDR)
+    sock.listen(1)
+
+    c_sock, _ = sock.accept()  # 只接受一个客户端
+
+
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+
+    loop.add_reader(c_sock, print_read_data, c_sock, loop)  # 监控客户端描述符的读事件,出现读事件触发回调函数
+    loop.call_at(loop.time()+50, stop_loop, loop, c_sock)  # 在50s之后停掉主事件循环
+
+    try:
+        loop.run_forever()
+    finally:
+        sock.close()
+        loop.close()
+
+
+if __name__ == '__main__':
+    """
+    可以使用 telnet localhost 8003 进行测试.
+    """
+    main()
