2770f1c35c57da275df53b839d22eb1c2289bbbe
康鑫 <k506855660@163.com>
2018-01-18 23:51:33 +0800

    “添加协程使用例子”

---
+++ parallel_hello_world.py
@@ -0,0 +1,40 @@
+import asyncio
+from functools import partial
+
+async def hello_world1():
+    while True:
+        print('hello_wrold1')
+        try:
+            await asyncio.sleep(1)
+        except asyncio.CancelledError:  #捕获删除协程的exception, 跳出while
+            break
+
+async def hello_world2():
+    while True:
+        print('hello_world2')
+        try:
+            await asyncio.sleep(1)
+        except asyncio.CancelledError:
+            break
+
+
+def del_task(*tasks):  # 删除掉tasks
+    for task in tasks:
+        task.cancel()
+
+
+loop = asyncio.get_event_loop()
+loop.set_debug(True)
+
+task1 = asyncio.ensure_future(hello_world1())  # 把helloworld协程加入主事件循环进行调度
+task2 = asyncio.ensure_future(hello_world2())
+loop.call_at(loop.time()+5, del_task, task1, task2)  # 5s之后删除两个helloworld协程
+
+try:
+    loop.run_until_complete(asyncio.gather(task1, task2))  #等待两个协程结束
+finally:
+    loop.stop()
+    loop.close()
+
+
+
--- watch_fd.py
+++ watch_fd.py
@@ -21,13 +21,10 @@ def main():
     sock = socket.socket()
     sock.bind(ADDR)
     sock.listen(1)
-
     c_sock, _ = sock.accept()  # 只接受一个客户端
-
 
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
-
     loop.add_reader(c_sock, print_read_data, c_sock, loop)  # 监控客户端描述符的读事件,出现读事件触发回调函数
     loop.call_at(loop.time()+50, stop_loop, loop, c_sock)  # 在50s之后停掉主事件循环
 
