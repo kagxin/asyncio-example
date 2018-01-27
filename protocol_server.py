77edf10e5ca1e4dbd8f5cb7676747d22ed29ff0f
康鑫 <k506855660@163.com>
2018-01-27 18:12:21 +0800

    protocol server ok

--- protocol_server.py
+++ protocol_server.py
@@ -1,40 +1,41 @@
 import asyncio
 import time
-import sys
 
-OUTTIME = 5
+OUTTIME = 10
 clients = {}
 
 
 class ProtocolServer(asyncio.Protocol):
 
     def connection_made(self, transport):
-        peername = transport.get_extra_info('peername')
-        clients.setdefault(transport, time.time()+OUTTIME)
         self.transport = transport
+        clients.setdefault(transport, time.time()+OUTTIME)
 
     def data_received(self, data):
-        message = data.decode('utf8')
-        self.transport.write(message)
+        clients.update({self.transport: time.time()+OUTTIME})  # 一旦接受到数据就更新timeout时间
+        message = data.decode()
+        self.transport.write(message.encode())
 
 
-async def check_alive():
+async def check_alive():  # 如果timeout就通过对应的transport将客户端关闭
     while True:
-        await asyncio.sleep(1)
-        for transport, timeout in clients.items():
-            if timeout > time.time():
+        await asyncio.sleep(0.3)
+        for transport, timeout in clients.copy().items():
+            if timeout < time.time():
+                print(transport, timeout, time.time())
                 transport.close()
+                clients.pop(transport)
 
 
 def main():
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
-    coro = loop.create_server(ProtocolServer, '127.0.0.1', 8888)
-    asyncio.ensure_future(check_alive())
-    # asyncio.ensure_future(coro)
+    coro = loop.create_server(ProtocolServer, '127.0.0.1', 8881)
+    asyncio.ensure_future(coro)
+    asyncio.ensure_future(check_alive(), loop=loop)
+
     try:
-        # loop.run_forever(coro)
-        loop.run_until_complete(coro)
+        loop.run_forever()
     except Exception as e:
         print(e)
     finally:
